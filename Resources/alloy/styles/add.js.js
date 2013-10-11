64b7c8df52572af340eec29c0e0e7f6758075c15
euforic <cs@euforic.co>
2013-10-11 16:38:49 -0700

    [TIMOB-15274]

--- Resources/alloy/styles/add.js.js
+++
@@ -1,1 +0,0 @@
-module.exports = [{"isClass":true,"priority":10000.0002,"key":"container","style":{backgroundColor:"white",}}];
--- Resources/alloy/styles/done.js
+++
@@ -1,1 +0,0 @@
-module.exports = [{"isClass":true,"priority":10000.0009,"key":"container","style":{backgroundColor:"white",}}];
